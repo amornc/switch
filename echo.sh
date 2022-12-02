f933e606d32e2b51068f45a1dbc57dfe6fb9b2cc
amornc <mornster@gmail.com>
2022-12-02 10:50:40 +0700

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -2,7 +2,8 @@ file="repo.txt"
 
 while read -r line; do
     echo "$line"
-    echo "export $line" >> $BASH_ENV
+    export "$line"
+#    echo "export $line" >> $BASH_ENV
 #    echo $BASH_ENV
 #    source $BASH_ENV
 done <$file
