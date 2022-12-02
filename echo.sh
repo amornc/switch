ed3f0c46a9aac4d070dc2d997d4d7bd2270c0739
amornc <mornster@gmail.com>
2022-12-02 14:07:54 +0700

    test script

--- echo.sh
+++ echo.sh
@@ -2,9 +2,5 @@ file="repo.txt"
 
 while read -r line; do
     echo "$line"
-    echo "export repo=$line" >> "$BASH_ENV"
-#    export repo="$line"
-#    echo "$repo" >> "$BASH_ENV"
-    echo "$BASH_ENV"
-#    source "$BASH_ENV"
+
 done <$file
